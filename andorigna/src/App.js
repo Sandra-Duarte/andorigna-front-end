96757e80ec8ec53c3cda00f9f55784f8311af322
Sandra-Duarte <106591234+Sandra-Duarte@users.noreply.github.com>
2023-10-16 19:47:42 +0100

    create footer and layout page

--- andorigna/src/App.js
+++ andorigna/src/App.js
@@ -6,6 +6,20 @@ import {
   Route,
   createRoutesFromElements
 } from "react-router-dom";
+import Home from './component/page/Home/Home';
+import Contact from './component/page/Contact/Contact';
+import About from './component/page/About Us/About';
+
+const router = createBrowserRouter(createRoutesFromElements(
+
+  <Route element ={<Layout/>}>
+
+    <Route path="/" element={<Home/>}/>
+    <Route path="/contact" element={<Contact/>}/>
+    <Route path="/about" element={<About/>}/>
+
+  </Route>
+))
 
 function App() {
   return (
