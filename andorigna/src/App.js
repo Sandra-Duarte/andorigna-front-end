31d847195a6e12fa4b90123275337fa31c4fe7d7
Sandra-Duarte <106591234+Sandra-Duarte@users.noreply.github.com>
2023-10-21 18:12:52 +0100

    fixe erro

--- andorigna/src/App.js
+++ andorigna/src/App.js
@@ -9,18 +9,16 @@ import {
 import Home from './component/page/Home/Home';
 import Contact from './component/page/Contact/Contact';
 import About from './component/page/About Us/About';
+import Sign  from './component/page/Sign Out/Sign';
 import Layout from './component/page/Navbar/Layout';
 
 const router = createBrowserRouter(createRoutesFromElements(
 
   <Route element ={<Layout/>}>
-
     <Route path="/" element={<Home/>}/>
     <Route path="/contact" element={<Contact/>}/>
-    <Route path="/about Us" element={<About/>}/>
-    <Route path="/about Us" element={<About/>}/>
-
-
+    <Route path="/About Us" element={<About/>}/>
+    <Route path="/Sign" element={<Sign/>}/>
   </Route>
 ))
 
